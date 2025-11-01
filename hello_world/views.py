3fa1c166b1c28ebe710ff7535ae8b0ad33a888f4
Deniz <denizpandar@gmail.com>
2025-11-01 14:59:32 +0000

    Create about me

--- hello_world/views.py
+++ hello_world/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, World")
-    
+
+
+def about_me(request):
+    return HttpResponse("This would be the about page")
